b2e0b02b22177d37efa725fda82b8dbcd92b6cbe
Kevin Lee <78504440+leeked@users.noreply.github.com>
2021-11-18 00:42:01 -0500

    Implemented Login
    TODO:
    Registration
    Home page

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,85 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
 
 app = Flask(__name__)
 
+#Configure MySQL
+# Probably will have to change port for different machines
+conn = pymysql.connect(host='localhost',
+                       port=3306,
+                       user='root',
+                       password='',
+                       db='airsystem',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+# Index Route
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html')
 
+# Login Route
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+# User Login Authentication
+@app.route('/loginAuth', methods=['GET','POST'])
+def loginAuth():
+    #grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+	
+	#cursor used to send queries
+	cursor = conn.cursor()
+	if request.form.get('login_type') == 'on':
+		#executes query
+		query = 'SELECT * FROM airline_staff WHERE username = %s and staff_password = %s'
+		cursor.execute(query, (username, password))
+		#stores the results in a variable
+		data = cursor.fetchone()
+		#use fetchall() if you are expecting more than 1 data row
+		cursor.close()
+		error = None
+		if(data):
+			#creates a session for the the user
+			#session is a built in
+			session['username'] = username
+			return redirect(url_for('home'))
+		else:
+			#returns an error message to the html page
+			error = 'Invalid login or username'
+			return render_template('login.html', error=error)
+
+	else:
+		#executes query
+		query = 'SELECT * FROM customer WHERE email = %s and customer_password = %s'
+		cursor.execute(query, (username, password))
+		#stores the results in a variable
+		data = cursor.fetchone()
+		#use fetchall() if you are expecting more than 1 data row
+		cursor.close()
+		error = None
+		if(data):
+			#creates a session for the the user
+			#session is a built in
+			session['username'] = username
+			return redirect(url_for('home'))
+		else:
+			#returns an error message to the html page
+			error = 'Invalid login or username'
+			return render_template('login.html', error=error)
+
+# Register Route
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+# Register Home
+@app.route('/home')
+def home():
+	return "hello xd"
+
+app.secret_key = 'i dont know what this is'
 if __name__ == '__main__':
     app.run(debug=True)
