a2c8ba0369ca239d28a04d1778b7e461f4687a8c
Kevin Lee <78504440+leeked@users.noreply.github.com>
2021-11-19 18:36:39 -0500

    Implemented Register
    Can register as both Staff and Customer

--- webapp/app.py
+++ webapp/app.py
@@ -18,6 +18,11 @@ conn = pymysql.connect(host='localhost',
 def index():
     return render_template('index.html')
 
+
+"""
+LOGIN
+"""
+
 # Login Route
 @app.route('/login')
 def login():
@@ -34,7 +39,7 @@ def loginAuth():
 	cursor = conn.cursor()
 	if request.form.get('login_type') == 'on':
 		#executes query
-		query = 'SELECT * FROM airline_staff WHERE username = %s and staff_password = %s'
+		query = 'SELECT * FROM airline_staff WHERE username = %s and staff_password = MD5(%s)'
 		cursor.execute(query, (username, password))
 		#stores the results in a variable
 		data = cursor.fetchone()
@@ -53,7 +58,7 @@ def loginAuth():
 
 	else:
 		#executes query
-		query = 'SELECT * FROM customer WHERE email = %s and customer_password = %s'
+		query = 'SELECT * FROM customer WHERE email = %s and customer_password = MD5(%s)'
 		cursor.execute(query, (username, password))
 		#stores the results in a variable
 		data = cursor.fetchone()
@@ -70,12 +75,103 @@ def loginAuth():
 			error = 'Invalid login or username'
 			return render_template('login.html', error=error)
 
+
+"""
+REGISTRATION
+"""
+
 # Register Route
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('regchoice.html')
 
-# Register Home
+# Register as Staff
+@app.route('/staffreg')
+def staffreg():
+	return render_template('staffreg.html')
+
+# Register as User
+@app.route('/userreg')
+def userreg():
+	return render_template('userreg.html')
+
+# Register Authentication 
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+	# Determine what kind of user (staff or customer/user)
+	regtype = request.form['type']
+
+	# For Staff
+	if regtype == 'staff':
+		#grabs information from the forms
+		username = request.form['username']
+		password = request.form['password']
+		fname = request.form['fname']
+		lname = request.form['lname']
+		DoB = request.form['DoB']
+
+		#cursor used to send queries
+		cursor = conn.cursor()
+		#executes query
+		query = 'SELECT * FROM airline_staff WHERE username = %s'
+		cursor.execute(query, (username))
+		#stores the results in a variable
+		data = cursor.fetchone()
+		#use fetchall() if you are expecting more than 1 data row
+		error = None
+		if(data):
+			#If the previous query returns data, then user exists
+			error = "This user already exists"
+			return render_template('staffreg.html', error = error)
+		else:
+			ins = "INSERT INTO airline_staff VALUES(%s, MD5(%s), %s, %s, %s)"
+			cursor.execute(ins, (username, password, fname, lname, DoB))
+			conn.commit()
+			cursor.close()
+			return render_template('index.html')
+
+	# For Customer/User
+	#grabs information from the forms
+	username = request.form['email']
+	name = request.form['name']
+	password = request.form['password']
+
+	addr_num = request.form['addr_building_num']
+	street = request.form['street']
+	city = request.form['city']
+	state = request.form['state']
+
+	phone = request.form['phone']
+
+	pass_num = request.form['passport_num']
+	pass_exp = request.form['passport_exp']
+	pass_country = request.form['passport_country']
+
+	DoB = request.form['DoB']
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM customer WHERE email = %s'
+	cursor.execute(query, (username))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This user already exists"
+		return render_template('userreg.html', error = error)
+	else:
+		ins = 'INSERT INTO customer VALUES(%s, %s, MD5(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+		cursor.execute(ins, (username, name, password, addr_num, street, city, state, phone, pass_num, pass_exp, pass_country, DoB))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
+
+
+
+# Home Route
 @app.route('/home')
 def home():
 	return "hello xd"
